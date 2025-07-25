a2b790d41db67742bfa86571a4af78d3f87cdb62
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2025-07-26 03:45:25 +0530

    fix: made a few tweaks to get better mouse interaction

--- src/components/InteractiveGradient/InteractiveGradient.jsx
+++ src/components/InteractiveGradient/InteractiveGradient.jsx
@@ -5,11 +5,11 @@ import * as THREE from "three";
 import { vertexShader, fluidShader, displayShader } from "./shaders.js";
 
 const InteractiveGradient = ({
-  brushSize = 25.0,
-  brushStrength = 0.5,
-  distortionAmount = 2.5,
-  fluidDecay = 0.98,
-  trailLength = 0.8,
+  brushSize = 50.0,
+  brushStrength = 1,
+  distortionAmount = 3,
+  fluidDecay = 1.98,
+  trailLength = 1.8,
   stopDecay = 0.85,
   color1 = "#b8ffebcd7",
   color2 = "#6e3466",
